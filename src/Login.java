ca8c8e0ba7b06568aed013d59c2d4a86581eeaf0
Ivan <47345247+ivanmendozalawrence@users.noreply.github.com>
2023-05-22 23:13:21 +0800

    update

--- src/Login.java
+++ src/Login.java
@@ -9,32 +9,44 @@ import java.util.HashMap;
 
 public class Login {
     private JFrame frame;
-    private JLabel header;
     private JPanel panel;
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JButton loginButton;
     private HashMap<String, String> credentials;
-    private int attempts;
+    private int attempts = 4;
 
     public Login() {
         panel = new JPanel();
-        frame = new JFrame("Login");
+        frame = new JFrame("Login Screen");
         frame.setSize(420, 180);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        header = new JLabel("Login", SwingConstants.CENTER);
-        header.setPreferredSize(new Dimension(frame.getWidth(), 30));
-        frame.add(header, BorderLayout.NORTH);
+        frame.setLayout(new GridLayout(1, 0));
         panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 2));
-        panel.add(new JLabel("Username:"));
-        usernameField = new JTextField();
-        panel.add(usernameField);
-        panel.add(new JLabel("Password:"));
-        passwordField = new JPasswordField();
-        panel.add(passwordField);
-        panel.add(new JLabel());
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.LINE_END;
+        panel.add(new JLabel("Username"), c);
+        c.gridx = 1;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.LINE_START;
+        usernameField = new JTextField("", 20);
+        panel.add(usernameField, c);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.anchor = GridBagConstraints.LINE_END;
+        panel.add(new JLabel("Password"), c);
+        c.gridx = 1;
+        c.gridy = 1;
+        c.anchor = GridBagConstraints.LINE_START;
+        passwordField = new JPasswordField("", 20);
+        panel.add(passwordField, c);
+        c.gridx = 0;
+        c.gridy = 2;
+        c.gridwidth = 2;
+        c.anchor = GridBagConstraints.CENTER;
         loginButton = new JButton("Login");
         loginButton.setBorderPainted(false);
         loginButton.addActionListener(new ActionListener() {
@@ -42,8 +54,10 @@ public class Login {
                 login();
             }
         });
-        panel.add(loginButton);
-        frame.add(panel, BorderLayout.CENTER);
+        panel.add(loginButton, c);
+        frame.add(panel);
+        frame.setLocationRelativeTo(null);
+
         frame.setVisible(true);
 
         credentials = new HashMap<String, String>();
@@ -65,7 +79,8 @@ public class Login {
     }
 
     public void login() {
-        if (attempts >= 3) {
+        if (attempts == 1) {
+            System.out.println("Too many failed attempts, closing the application.");
             JOptionPane.showOptionDialog(frame, "Too many failed attempts. The application will now terminate.", "Error", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE, null, new Object[]{"OK"}, null);
             System.exit(0);
         }
@@ -77,10 +92,11 @@ public class Login {
             System.out.println("Login successful!");
             attempts = 0;
             new Records();
+            frame.dispose();
         } else {
             System.out.println("Login failed!");
-            attempts++;
-            JOptionPane.showMessageDialog(frame, "Invalid username or password. Please try again.");
+            attempts--;
+            JOptionPane.showMessageDialog(frame, "Invalid username or password. \n\t\t\tPlease try again "+attempts+" attempts left");
         }
     }
 }
--- src/Records.java
+++ src/Records.java
@@ -1,17 +1,12 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
+import java.util.*;
+import java.time.*;
 
-public class Records {
+class Records {
     private JFrame frame;
     private JTextArea textArea;
     private JComboBox<String> sortComboBox;
@@ -22,6 +17,13 @@ public class Records {
     private JButton exportButton;
     private ArrayList<String[]> records;
 
+    // Add Record window
+    private JFrame addFrame;
+    private JTextField nameField;
+    private JComboBox<String> monthComboBox;
+    private JComboBox<Integer> dayComboBox;
+    private JComboBox<Integer> yearComboBox;
+
     public Records() {
         frame = new JFrame("List of Records");
         frame.setSize(375, 400);
@@ -31,7 +33,7 @@ public class Records {
         JPanel recordPanel = new JPanel();
         recordPanel.setLayout(new BorderLayout());
 
-        textArea = new JTextArea(8, 0);
+        textArea = new JTextArea();
         textArea.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(textArea);
         recordPanel.add(scrollPane, BorderLayout.CENTER);
@@ -82,7 +84,7 @@ public class Records {
         addButton = new JButton("Add a Record");
         addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                addRecord();
+                showAddRecordWindow();
             }
         });
 
@@ -115,121 +117,273 @@ public class Records {
         frame.setVisible(true);
     }
 
-    public void addRecord() {
-        String name = JOptionPane.showInputDialog(frame, "Enter name:");
-        if (name == null || name.isEmpty()) return;
+    public void showAddRecordWindow() {
+        addFrame = new JFrame("Add Record");
+        addFrame.setSize(400, 200);
+        addFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        addFrame.setLayout(new BorderLayout());
 
-        String birthday = JOptionPane.showInputDialog(frame, "Enter birthday (MM/DD/YYYY):");
-        if (birthday == null || birthday.isEmpty()) return;
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
 
-        String ageStr = JOptionPane.showInputDialog(frame, "Enter age:");
-        if (ageStr == null || ageStr.isEmpty()) return;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.LINE_END;
+        inputPanel.add(new JLabel("Name:"), c);
 
-        int age;
-        try {
-            age = Integer.parseInt(ageStr);
-            if (age < 0) throw new NumberFormatException();
+        c.gridx++;
+        c.anchor = GridBagConstraints.LINE_START;
+        nameField = new JTextField("", 20);
+        inputPanel.add(nameField, c);
 
-            String[] rowData = {name, birthday, ageStr};
-            records.add(rowData);
+        c.gridx--;
+        c.gridy++;
+        c.anchor = GridBagConstraints.LINE_END;
+        inputPanel.add(new JLabel("Birthday:"), c);
 
-            updateTextArea();
+        c.gridx++;
+        c.anchor = GridBagConstraints.LINE_START;
+        JPanel datePanel = new JPanel();
+        datePanel.setLayout(new FlowLayout());
+        String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        monthComboBox = new JComboBox<String>(months);
+        datePanel.add(monthComboBox);
+        Integer[] days = new Integer[31];
+        for (int i = 1; i <= 31; i++) days[i - 1] = i;
+        dayComboBox = new JComboBox<Integer>(days);
+        datePanel.add(dayComboBox);
+        Integer[] years = new Integer[Year.now().getValue() - 1899];
+        for (int i = 1900; i <= Year.now().getValue(); i++) years[i - 1900] = i;
+        yearComboBox = new JComboBox<Integer>(years);
+        datePanel.add(yearComboBox);
+        inputPanel.add(datePanel, c);
 
-            JOptionPane.showMessageDialog(frame, "Record added.");
+        addFrame.add(inputPanel, BorderLayout.CENTER);
 
-            return;
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
 
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(frame, "Invalid age. Please enter a valid number.");
-            return;
+        JButton saveAndGoBackButton = new JButton("Save and Go Back");
+        saveAndGoBackButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (addRecord()) {
+                    addFrame.setVisible(false);
+                }
+            }
+        });
+        buttonPanel.add(saveAndGoBackButton);
+
+        JButton saveAndAddAnotherButton = new JButton("Save & Add Another");
+        saveAndAddAnotherButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                addRecord();
+            }
+        });
+        buttonPanel.add(saveAndAddAnotherButton);
+
+        JButton backButton = new JButton("Back");
+        backButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                addFrame.setVisible(false);
+            }
+        });
+        buttonPanel.add(backButton);
+
+        addFrame.add(buttonPanel, BorderLayout.SOUTH);
+
+        addFrame.setVisible(true);
+    }
+
+    public boolean addRecord() {
+        String name=nameField.getText().trim();
+        if(name.isEmpty()){
+            System.out.println("Please enter a name.");
+            JOptionPane.showMessageDialog(addFrame,"Please enter a name.");
+            return false;
         }
+
+        int month=monthComboBox.getSelectedIndex()+1;
+        int day=(int)dayComboBox.getSelectedItem();
+        int year=(int)yearComboBox.getSelectedItem();
+
+        LocalDate birthday;
+        try{
+            birthday=LocalDate.of(year,month,day);
+
+            if(birthday.isAfter(LocalDate.now())){
+                throw new IllegalArgumentException();
+            }
+
+        }catch(Exception e){
+            System.out.println("Invalid birthday. Please enter a valid date.");
+            JOptionPane.showMessageDialog(addFrame,"Invalid birthday. Please enter a valid date.");
+            return false;
+        }
+
+        long age=Period.between(birthday,LocalDate.now()).getYears();
+
+        String[] rowData={name,String.format("%02d/%02d/%04d",month,day,year),String.valueOf(age)};
+        records.add(rowData);
+
+        updateTextArea();
+
+        JOptionPane.showMessageDialog(addFrame,"Record added.");
+
+        return true;
     }
 
     public void removeRecord() {
-        if (records.size() == 0) {
-            JOptionPane.showMessageDialog(frame, "No records to remove.");
+        if(records.size()==0){
+            JOptionPane.showMessageDialog(frame,"No records to remove.");
             return;
         }
 
-        String[] options = new String[records.size()];
-        for (int i = 0; i < records.size(); i++) {
-            options[i] = records.get(i)[0];
+        JFrame removeFrame = new JFrame("Remove Record");
+        removeFrame.setSize(600, 200);
+        removeFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        removeFrame.setLayout(new BorderLayout());
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.LINE_END;
+        inputPanel.add(new JLabel("Name:"), c);
+
+        c.gridx++;
+        c.anchor = GridBagConstraints.LINE_START;
+        JTextField nameField = new JTextField("", 20);
+        inputPanel.add(nameField, c);
+
+        removeFrame.add(inputPanel, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+
+        JButton removeAndGoBackButton = new JButton("Remove and Go Back");
+        removeAndGoBackButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (removeRecord(nameField.getText().trim())) {
+                    removeFrame.setVisible(false);
+                }
+            }
+        });
+        buttonPanel.add(removeAndGoBackButton);
+
+        JButton saveAndRemoveAnotherButton = new JButton("Save & Remove Another");
+        saveAndRemoveAnotherButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                removeRecord(nameField.getText().trim());
+            }
+        });
+        buttonPanel.add(saveAndRemoveAnotherButton);
+
+        JButton backButton = new JButton("Back");
+        backButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                removeFrame.setVisible(false);
+            }
+        });
+        buttonPanel.add(backButton);
+
+        removeFrame.add(buttonPanel, BorderLayout.SOUTH);
+
+        removeFrame.setVisible(true);
+    }
+
+    private boolean removeRecord(String name) {
+        if(name.isEmpty()){
+            JOptionPane.showMessageDialog(frame,"Please enter a name.");
+            return false;
         }
 
-        String name = (String) JOptionPane.showInputDialog(frame, "Select record to remove:", "Remove Record", JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+        boolean found = false;
 
-        if (name == null || name.isEmpty()) return;
-
-        for (int i = 0; i < records.size(); i++) {
-            if (records.get(i)[0].equals(name)) {
+        for(int i=0;i<records.size();i++){
+            if(records.get(i)[0].equals(name)){
                 records.remove(i);
+                found = true;
                 break;
             }
         }
 
-        updateTextArea();
+        if(found){
+            updateTextArea();
+            JOptionPane.showMessageDialog(frame,"Record removed.");
+            return true;
 
-        JOptionPane.showMessageDialog(frame, "Record removed.");
+        }else{
+            JOptionPane.showMessageDialog(frame,"An IllegalArgumentException Caught: Name Not Found!");
+            return false;
+
+        }
     }
 
     public void exportToCSV() {
-        if (records.size() == 0) {
-            JOptionPane.showMessageDialog(frame, "No records to export.");
+        if(records.size()==0){
+            System.out.println("No Records");
+            JOptionPane.showMessageDialog(frame,"No records to export.");
             return;
         }
 
-        String filename = getCurrentDateTime() + ".csv";
+        String filename=getCurrentDateTime()+".csv";
 
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+        try{
+            BufferedWriter writer=new BufferedWriter(new FileWriter(filename));
 
             writer.write("NAMES,BIRTHDAY,AGE\n");
 
-            for (String[] record : records) {
-                writer.write(record[0] + "," + record[1] + "," + record[2] + "\n");
+            for(String[] record:records){
+                writer.write(record[0]+","+record[1]+","+record[2]+"\n");
             }
 
             writer.close();
 
-            JOptionPane.showMessageDialog(frame, "Records exported to " + filename + ".");
+            JOptionPane.showMessageDialog(frame,"Records exported to "+filename+".");
 
-        } catch (IOException e) {
+            System.out.println("Records successful exported to "+filename+".");
+
+        }catch(IOException e){
             e.printStackTrace();
-            JOptionPane.showMessageDialog(frame, "Error exporting records.");
+            JOptionPane.showMessageDialog(frame,"Error exporting records.");
         }
     }
 
-    private void updateTextArea() {
+    private void updateTextArea(){
         textArea.setText("");
 
-        int sortColumn = sortComboBox.getSelectedIndex();
+        int sortColumn=sortComboBox.getSelectedIndex();
 
-        Collections.sort(records, new Comparator<String[]>() {
-            public int compare(String[] a, String[] b) {
-                if (sortColumn == 2) {
-                    int ageA = Integer.parseInt(a[2]);
-                    int ageB = Integer.parseInt(b[2]);
-                    return ageA - ageB;
-                } else {
+        Collections.sort(records,new Comparator<String[]>(){
+            public int compare(String[] a,String[] b){
+                if(sortColumn==2){
+                    int ageA=Integer.parseInt(a[2]);
+                    int ageB=Integer.parseInt(b[2]);
+                    return ageA-ageB;
+                }else{
                     return a[sortColumn].compareTo(b[sortColumn]);
                 }
             }
         });
 
-        if (descendingButton.isSelected()) {
+        if(descendingButton.isSelected()){
             Collections.reverse(records);
         }
 
         textArea.append("\tName\tBirthdate\tAge\t\n");
 
-        for (String[] record : records) {
-            textArea.append("\t" + record[0] + "\t" + record[1] + "\t" + record[2] + "\t\n");
+        for(String[] record:records){
+            textArea.append("\t"+record[0]+"\t"+record[1]+"\t"+record[2]+"\t\n");
         }
     }
 
-    private String getCurrentDateTime() {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-        Date date = new Date(System.currentTimeMillis());
+    private String getCurrentDateTime(){
+        SimpleDateFormat formatter=new SimpleDateFormat("yyyyMMddHHmmss");
+        Date date=new Date(System.currentTimeMillis());
         return formatter.format(date);
     }
 }
+
